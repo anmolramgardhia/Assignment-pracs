9389cacbf9e1daf1a3de6e20e8e6a89210f0be38
anmolramgardhia <anmolramgardhia@gmail.com>
2026-04-23 12:39:54 +0530

    made the base of nlp

--- NLP FUNDAMENTALS.py
+++ NLP FUNDAMENTALS.py
@@ -1,1 +1,14 @@
+import numpy as np 
+
+def relu(x):
+    return np.maximum(0, x)
+
+def forward_pass(x, params):
+    W1 , b1 , W2 , b2 = params
+    z1 = x@ W1 + b1
+    h1 = relu(z1)
+    z2 = h1 @ W2 + b2
+    
+    return z1 , h1 , z2
+
 
