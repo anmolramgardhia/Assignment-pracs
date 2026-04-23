8ff2337cc4ee2b1b2b7bfa551e3db9d894880514
anmolramgardhia <anmolramgardhia@gmail.com>
2026-04-23 12:48:31 +0530

    made tha last changes in the base structure

--- NLP FUNDAMENTALS.py
+++ NLP FUNDAMENTALS.py
@@ -11,4 +11,12 @@ def forward_pass(x, params):
     
     return z1 , h1 , z2
 
+x_batch = np.array([[0.5, -1.0, 2.0], [1.5, 0.2, -0.3]])
+params = (
+    np.array([[0.2, -0.4], [0.7, 0.5], [-0.3, 0.8]]),  # W1
+    np.array([0.1, -0.2]),                             # b1
+    np.array([[0.6, -0.1], [-0.5, 0.9]]),              # W2
+    np.array([0.0, 0.05]))                             # b2
 
+z1, h1, z2 = forward_pass(x_batch, params)
+print(z1 , h1 , z2) 
